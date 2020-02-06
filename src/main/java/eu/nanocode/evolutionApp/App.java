1ac741bb989bfad5cc285af6cf1d002e938fa81f
Łukasz Majchrzycki <lukasz.majchrzycki@gmail.com>
2020-02-06 16:06:36 +0100

    configure Maven plugin for fat jar generation

---
+++ src/main/java/eu/nanocode/evolutionApp/App.java
@@ -0,0 +1,33 @@
+package eu.nanocode.evolutionApp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root;
+        Scene scene;
+        URL url = getClass().getResource("evolutionApp.fxml");
+        root = FXMLLoader.load(url);
+
+        primaryStage.setTitle("Evolution");
+        scene= new Scene(root, 700, 600);
+        scene.getStylesheets().add(getClass().getResource("graph.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+}
--- src/main/java/eu/nanocode/evolutionApp/Main.java
+++ src/main/java/eu/nanocode/evolutionApp/Main.java
@@ -1,33 +1,10 @@
 package eu.nanocode.evolutionApp;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+public class Main {
 
-import java.net.URL;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root;
-        Scene scene;
-        URL url = getClass().getResource("evolutionApp.fxml");
-        root = FXMLLoader.load(url);
-
-        primaryStage.setTitle("Evolution");
-        scene= new Scene(root, 700, 600);
-        scene.getStylesheets().add(getClass().getResource("graph.css").toExternalForm());
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public static void main(String[] args)   {
+        App.main(args);
 
     }
 
-
-    public static void main(String[] args) {
-
-        launch(args);
-    }
 }
